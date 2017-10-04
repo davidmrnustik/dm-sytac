62786013e6104f11e85243ac08cd07dafcaa0120
David Mrnustik <david.mrnustik@hotmail.com>
2017-10-04 21:45:26 +0200

    New configuration, structure, code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Header</h1>
       </div>
     );
   }
 }
-
-// this is a test comment
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, 'src/index.html'),
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].css',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        exclude: [
+          path.resolve(__dirname, "node_modules")
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ["env"]
+        }
+      },
+      {
+        test: /\.scss?$/,
+        use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          },{
+            loader: "sass-loader"
+          }],
+          fallback: "style-loader"
+        })
+
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    HtmlWebpackPluginConfig
+  ]
+}
